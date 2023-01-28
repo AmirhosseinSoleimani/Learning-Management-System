a70bcca22f1b7c50d45fbca2e956e33644b868a5
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2023-01-28 14:11:29 +0330

    Implementation MyLearning Screens Structure

---
+++ lib/presentation/widgets/navigation_bar_widgets/my_learning.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyLearning extends StatelessWidget {
+  const MyLearning({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text(
+          'MyLearning'
+      ),
+    );
+  }
+}
