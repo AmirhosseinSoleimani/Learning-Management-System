644d9b4b37d1450b1b092350dac2a7e4530c72dd
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2023-01-28 17:42:53 +0330

    Implementation Navigation Bar ActivationIcon in AppIcon to Resources of Presentation Layer

--- lib/presentation/resources/icon_manager.dart
+++ lib/presentation/resources/icon_manager.dart
@@ -3,8 +3,15 @@ import 'package:flutter/material.dart';
 class AppIcon{
   // Navigation bar Icons
   static const IconData home = Icons.home_outlined;
-  static const IconData explore = Icons.search_rounded;
+  static const IconData explore = Icons.search_outlined;
   static const IconData myLearning = Icons.slow_motion_video_outlined;
   static const IconData wishlist = Icons.favorite_border_outlined;
   static const IconData account = Icons.account_circle_outlined;
+
+  // Navigation bar ActivationIcon
+  static const IconData homeActive = Icons.home;
+  static const IconData exploreActive = Icons.search;
+  static const IconData myLearningActive = Icons.slow_motion_video;
+  static const IconData wishlistActive = Icons.favorite_border;
+  static const IconData accountActive = Icons.account_circle;
 }
