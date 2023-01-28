493c8efa0920a6b45dbd5ef5744b7f695014d05c
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2023-01-28 14:10:56 +0330

    Implementation Wishlist Screens Structure

---
+++ lib/presentation/widgets/navigation_bar_widgets/wishlist.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Wishlist extends StatelessWidget {
+  const Wishlist({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text(
+          'Wishlist'
+      ),
+    );
+  }
+}
