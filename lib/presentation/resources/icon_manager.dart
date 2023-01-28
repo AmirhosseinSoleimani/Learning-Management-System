a2511b9881da48d2f9b76df2340863e94a4e35e1
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2023-01-28 13:44:25 +0330

    Implementation NavigationBar Icons in  Icon_manager of Project

---
+++ lib/presentation/resources/icon_manager.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Icon{
+  // Navigation bar Icons
+  static const IconData home = Icons.home_outlined;
+  static const IconData explore = Icons.search_rounded;
+  static const IconData myLearning = Icons.slow_motion_video_outlined;
+  static const IconData wishlist = Icons.favorite_border_outlined;
+  static const IconData account = Icons.account_circle_outlined;
+}
