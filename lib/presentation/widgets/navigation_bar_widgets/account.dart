c42bd3703d8e92532c7076042b2561cdeb09822d
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2023-01-28 14:12:02 +0330

    Implementation Account Screens Structure

---
+++ lib/presentation/widgets/navigation_bar_widgets/account.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Account extends StatelessWidget {
+  const Account({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text(
+          'Account'
+      ),
+    );
+  }
+}
