0e7905b821698f511ab0225d8634765d69769ae4
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2023-01-28 13:51:33 +0330

    Implementation NavigationBar String in  String_manager of Project

---
+++ lib/presentation/resources/string_manager.dart
@@ -0,0 +1,9 @@
+
+class AppString{
+  // Navigation Bar String
+  static const String home = 'Home';
+  static const String explore = 'Explore';
+  static const String myLearning = 'My Learning';
+  static const String wishlist = 'Wishlist';
+  static const String account = 'Account';
+}
