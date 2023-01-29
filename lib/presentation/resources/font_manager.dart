994cb87e4dedb45c776f446bc643823c830229cf
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2023-01-29 18:51:07 +0330

    Implementation FontWeightManager Class in Font Manager of Presentation Layer

---
+++ lib/presentation/resources/font_manager.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class FontConstants{
+  static const String fontFamily = 'Source Sans Pro';
+}
+
+class FontWeightManager{
+  static const FontWeight extraLight = FontWeight.w200;
+  static const FontWeight light = FontWeight.w300;
+  static const FontWeight regular = FontWeight.w400;
+  static const FontWeight semiBold = FontWeight.w600;
+  static const FontWeight bold = FontWeight.w700;
+  static const FontWeight extraBold = FontWeight.w900;
+}
+
+class FontSize{
+  static const double s12 = 12.0;
+  static const double s14 = 14.0;
+  static const double s16 = 16.0;
+  static const double s17 = 17.0;
+  static const double s18 = 18.0;
+  static const double s20 = 20.0;
+
+}
