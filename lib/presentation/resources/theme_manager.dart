04a02970376adc3e94e583848d1820cc805b5641
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2023-01-31 22:51:26 +0330

    Apply Value Manager in Theme Manager

--- lib/presentation/resources/theme_manager.dart
+++ lib/presentation/resources/theme_manager.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:learning_management_system/presentation/resources/color_manager.dart';
 import 'package:learning_management_system/presentation/resources/style_manager.dart';
+import 'package:learning_management_system/presentation/resources/values_manager.dart';
 
 ThemeData getApplicationTheme(){
   return ThemeData(
     textTheme: TextTheme(
-      labelMedium: getRegularStyle(fontSize: 14, color: ColorManager.white),
-      bodyText1: getRegularStyle(fontSize: 14, color: ColorManager.white)
+      labelMedium: getRegularStyle(fontSize: AppSize.s14, color: ColorManager.white),
+      bodyText1: getRegularStyle(fontSize: AppSize.s14, color: ColorManager.white)
     ),
     bottomNavigationBarTheme: BottomNavigationBarThemeData(
-      selectedLabelStyle: getRegularStyle(fontSize: 14, color: ColorManager.white),
-      unselectedLabelStyle: getRegularStyle(fontSize: 14,color: ColorManager.whiteWhitOpacity60)
+      selectedLabelStyle: getRegularStyle(fontSize: AppSize.s14, color: ColorManager.white),
+      unselectedLabelStyle: getRegularStyle(fontSize: AppSize.s14,color: ColorManager.whiteWhitOpacity60)
     ),
     appBarTheme: AppBarTheme(
-      titleTextStyle: getSemiBoldStyle(fontSize: 16, color: ColorManager.white),
+      titleTextStyle: getSemiBoldStyle(fontSize: AppSize.s18, color: ColorManager.white),
       backgroundColor: ColorManager.black,
     )
   );
