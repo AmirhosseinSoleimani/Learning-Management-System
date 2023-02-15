ae872a62434172b19fdeba6c8517685f37d9b77e
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2023-02-15 12:51:53 +0330

    Add MultiProvider in app in App Layer

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:learning_management_system/app/provider/navigationBar/navigationBar_provider.dart';
 import 'package:learning_management_system/presentation/bottom_navigation/navigation_cubit.dart';
 import 'package:learning_management_system/presentation/bottom_navigation/navigation_screen.dart';
 import 'package:learning_management_system/presentation/resources/theme_manager.dart';
+import 'package:provider/provider.dart';
 
+import '../presentation/authentication/sign_up/sign_up_bloc.dart';
+import '../presentation/bottom_navigationBar/navigation_screen.dart';
 import '../presentation/resources/route_manager.dart';
 
 
@@ -23,15 +27,16 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<NavigationCubit>(
-      create: (context) => NavigationCubit(),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: getApplicationTheme(),
-        onGenerateRoute: RouteGenerator.getRoute,
-        initialRoute: Routes.navigationBar,
-        home: const NavigationScreen(),
-      ),
-    );
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (context) => NavigationBarProvider()),
+        ],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: getApplicationTheme(),
+          onGenerateRoute: RouteGenerator.getRoute,
+          initialRoute: Routes.navigationBar,
+          home: const NavigationBarScreen(),
+        ),);
   }
 }
