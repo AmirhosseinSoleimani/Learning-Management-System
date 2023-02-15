44b69a2067c1a99563f4a3a8acda6b9c0dd4234e
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2023-02-15 12:38:15 +0330

    Add customNavigationBar Widget to Widgets and Modify glassButton Widget

--- lib/presentation/widgets/widgets.dart
+++ lib/presentation/widgets/widgets.dart
@@ -1,27 +1,48 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:learning_management_system/presentation/resources/values_manager.dart';
+import '../resources/string_manager.dart';
 
-Widget glassButton(Widget child,double start,double end,BuildContext context){
+Widget glassButton(Widget child,double startOpacity,double endOpacity,BuildContext context,double heightSize,double radiusSize){
     return ClipRRect(
       child: BackdropFilter(
         filter: ImageFilter.blur(sigmaX: AppSize.s4, sigmaY: AppSize.s4),
         child: Container(
           width: MediaQuery.of(context).size.width,
-          height: AppSize.s60,
+          height: heightSize,
           decoration: BoxDecoration(
             gradient: LinearGradient(
               colors: [
-                Colors.pinkAccent.withOpacity(start),
-                Colors.purpleAccent.withOpacity(end),
+                Colors.pinkAccent.withOpacity(startOpacity),
+                Colors.purpleAccent.withOpacity(endOpacity),
               ],
               begin: AlignmentDirectional.topStart,
               end: AlignmentDirectional.bottomEnd,
             ),
-            borderRadius: const BorderRadius.all(Radius.circular(AppSize.s30)),
+            borderRadius: BorderRadius.all(Radius.circular(radiusSize)),
           ),
           child: child,
         ),
       ),
     );
+}
+
+
+BottomNavigationBarItem customNavigationBar(Color color,double iconSize,String svgPicturePath){
+  return BottomNavigationBarItem(
+    icon: SvgPicture.asset(
+      svgPicturePath,
+      color: color,
+      width: iconSize,
+      height: iconSize,
+    ),
+    label: AppStringNavigationBar.home,
+    activeIcon: SvgPicture.asset(
+      svgPicturePath,
+      color: color,
+      width: iconSize,
+      height: iconSize,
+    ),
+  );
 }
