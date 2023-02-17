8dd9b9bf91c71822ad76378e3795089060c1cbdd
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2023-02-18 01:43:46 +0330

    Add CategoriesList in HomePageWidget of NavigationBarScreen in Presentation Layer as Local

---
+++ lib/presentation/constants/categories_list.dart
@@ -0,0 +1,13 @@
+const List<String> categoriesList = [
+  'Development',
+  'Finance &  Accounting',
+  'Office Productivity',
+  'Business',
+  'IT & Software',
+  'Design',
+  'Marketing',
+  'Lifestyle',
+  'Music',
+  'Health & Fitness',
+  'Teaching & Academics',
+];
