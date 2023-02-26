1a24fded4aea3bb4f23646ba4e57060679347647
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2023-02-26 20:38:39 +0330

    Implementation Category Model in Domain Layer

---
+++ lib/domain/category_model/category_model.dart
@@ -0,0 +1,25 @@
+class CategoryModel{
+  String? id;
+  String? name;
+  String? picture;
+
+  CategoryModel({
+    this.id,
+    this.name,
+    this.picture,
+  });
+
+  CategoryModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    picture = json['picture'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['id'] = id;
+    data['name'] = name;
+    data['picture'] = picture;
+    return data;
+  }
+}
