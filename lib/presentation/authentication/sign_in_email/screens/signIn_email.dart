c13e548d44c80677e81db185d206165dafa939e2
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2023-03-10 16:21:12 +0330

    Adding AppBar of SignInWithEmailScreen in Presentation Layer

--- lib/presentation/authentication/sign_in_email/screens/signIn_email.dart
+++ lib/presentation/authentication/sign_in_email/screens/signIn_email.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:learning_management_system/presentation/resources/color_manager.dart';
+
+import '../../../widgets/widgets.dart';
 
 class SignInWithEmailScreen extends StatelessWidget {
   const SignInWithEmailScreen({Key? key}) : super(key: key);
@@ -6,7 +9,16 @@ class SignInWithEmailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('hhhh'),
+      backgroundColor: ColorManager.black,
+      appBar: AppBar(
+        leading: arrowBackButton(context: context),
+      ),
+      body: Column(
+        children: [
+          Text('Welcome back!'),
+          Text('Enter your email to log in youe account'),
+        ],
+      ),
     );
   }
 }
